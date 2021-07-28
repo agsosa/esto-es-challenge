38e9eb1e2b4249e7eee336caa63f1f56d819c5db
Alejandro Sosa <agsosa74@gmail.com>
2021-07-28 17:30:10 -0300

    Move shared layout to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,27 @@
 import '@styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
+import Footer from '@components/layout/Footer';
+import Header from '@components/layout/Header';
+import tw from 'twin.macro';
+
+const Container = tw.div`flex flex-col h-screen`;
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Head>
+        <title>My Projects - Esto Es</title>
+        <meta name='description' content='Frontend Challenge para Esto Es' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Container>
+        <Header />
+
+        <Component {...pageProps} />
+
+        <Footer />
+      </Container>
     </ChakraProvider>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,27 @@
-import Head from 'next/head';
-import tw from 'twin.macro';
-import Footer from '@components/layout/Footer';
-import Header from '@components/layout/Header';
+import Main from '@components/layout/Main';
 import Nav from '@components/layout/Nav';
 import ProjectsTable from '@components/projects/display/Table';
 import ProjectsList from '@components/projects/display/List';
-
-const Container = tw.div`flex flex-col h-screen`;
-const Main = tw.main`bg-silver-500 w-full h-full py-8 px-12`;
+import { AiOutlinePlus } from 'react-icons/ai';
+import Button from '@components/common/Button';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  return (
-    <Container>
-      <Head>
-        <title>My Projects - Esto Es</title>
-        <meta name='description' content='Frontend Challenge para Esto Es' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+  const router = useRouter();
 
-      <Header />
-      <Nav />
+  const handleAddProject = () => router.push('/project/add');
+
+  return (
+    <>
+      <Nav
+        title='My projects'
+        rightComponent={<Button onClick={handleAddProject} icon={<AiOutlinePlus size='20px' />} label='Add project' />}
+      />
 
       <Main>
         <ProjectsTable />
         <ProjectsList />
       </Main>
-
-      <Footer />
-    </Container>
+    </>
   );
 }
