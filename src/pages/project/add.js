27971b5d71603c855e1ba82dacdbb00f45280f02
Alejandro Sosa <agsosa74@gmail.com>
2021-07-28 17:30:22 -0300

    Install dependencies & initialize form pages

---
+++ src/pages/project/add.js
@@ -0,0 +1,12 @@
+import Main from '@components/layout/Main';
+import Nav from '@components/layout/Nav';
+
+export default function ProjectForm() {
+  return (
+    <>
+      <Nav useBack title="Add project" />
+
+      <Main>xd</Main>
+    </>
+  );
+}
