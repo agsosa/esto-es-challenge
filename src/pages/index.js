d74df9d264b91e1ef01fc028be6b0e2d4c417f76
Alejandro Sosa <agsosa74@gmail.com>
2021-07-28 13:43:59 -0300

    Create Footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import tw from 'twin.macro';
 import Button from '@components/common/Button';
+import Footer from '@components/layout/Footer';
+
 import { AiOutlinePlus }  from 'react-icons/ai';
 
 const Container = tw.div`flex flex-col h-screen`;
@@ -9,7 +10,6 @@ const Header = tw.header`bg-white p-4 border-b`;
 const Nav = tw.nav`bg-white p-4 shadow-xl border-b flex justify-between items-center`;
 
 const NavTitle = tw.h1`font-semibold text-xl`;
-const Footer = tw.footer`bg-gray-200 border-t w-full p-10 flex items-center justify-between`;
 const Main = tw.main`bg-silver-500 w-full h-full`;
 
 export default function Home() {
@@ -29,9 +29,7 @@ export default function Home() {
 
       <Main>xd</Main>
 
-      <Footer>
-       Alejandro Sosa © 2021
-      </Footer>
+      <Footer />
     </Container>
   );
 }
