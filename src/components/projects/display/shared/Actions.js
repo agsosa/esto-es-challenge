c53c4b21f21ba4742fe1873dc153409b0464989b
Alejandro Sosa <agsosa74@gmail.com>
2021-07-28 19:13:23 -0300

    Add actions popover

--- src/components/projects/display/shared/Actions.js
+++ src/components/projects/display/shared/Actions.js
@@ -1,8 +1,59 @@
+import { useState } from 'react';
 import tw from 'twin.macro';
-import { HiDotsVertical } from 'react-icons/hi';
+import { HiDotsVertical, HiOutlinePencilAlt } from 'react-icons/hi';
+import { AiOutlineDelete } from 'react-icons/ai';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+} from '@chakra-ui/react';
 
-const MenuBtn = tw(HiDotsVertical)`text-2xl`;
+const MenuBtn = tw.button`text-2xl`;
+
+const ItemList = tw.ul`flex flex-col`;
+const Item = tw.li`flex space-x-2 items-center border-b p-3 w-full cursor-pointer transition duration-500 hover:bg-gray-100`;
 
 export default function Actions() {
-  return <MenuBtn />;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => setIsOpen(!isOpen);
+  const close = () => setIsOpen(false);
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = () => {
+    
+  }
+
+  return (
+    <>
+      <Popover returnFocusOnClose={false} isOpen={isOpen} onClose={close} placement='bottom' closeOnBlur closeOnEsc>
+        <PopoverTrigger>
+          <MenuBtn onClick={open}>
+            <HiDotsVertical />
+          </MenuBtn>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow />
+          <ItemList>
+            <Item onClick={handleEdit}>
+              <HiOutlinePencilAlt />
+              <span>Edit</span>
+            </Item>
+            <Item onClick={handleDelete}>
+              <AiOutlineDelete />
+              <span>Delete</span>
+            </Item>
+          </ItemList>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
 }
