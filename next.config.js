b3026e668a0f8734097175937de87d34a61966fa
Alejandro Sosa <agsosa74@gmail.com>
2021-07-29 16:35:52 -0300

    Ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   webpack5: true ,
   reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
   webpack: config => {
     // Unset client-side javascript that only works server-side
     config.resolve.fallback = { fs: false, module: false }
